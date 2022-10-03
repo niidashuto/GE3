beaaf04cbb5589a000b92fafc9676c39da63479f
niida_shuto <k021g1267@g.neec.ac.jp>
2022-10-03 14:20:22 +0900

    01_02まで

--- main.cpp
+++ main.cpp
@@ -649,7 +649,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     // 頂点シェーダの読み込みとコンパイル
     result = D3DCompileFromFile(
-        L"BasicVS.hlsl",  // シェーダファイル名
+        L"Resources/shaders/BasicVS.hlsl",  // シェーダファイル名
         nullptr,
         D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
         "main", "vs_5_0", // エントリーポイント名、シェーダーモデル指定
@@ -673,7 +673,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     // ピクセルシェーダの読み込みとコンパイル
     result = D3DCompileFromFile(
-        L"BasicPS.hlsl",   // シェーダファイル名
+        L"Resources/shaders/BasicPS.hlsl",   // シェーダファイル名
         nullptr,
         D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
         "main", "ps_5_0", // エントリーポイント名、シェーダーモデル指定
