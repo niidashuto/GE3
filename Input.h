1a620ce81df843acb139fb617b982978404eb99b
niida_shuto <k021g1267@g.neec.ac.jp>
2022-10-03 15:56:57 +0900

    02_02完了

--- Input.h
+++ Input.h
@@ -1,13 +1,23 @@
 #pragma once
 
 #include <Windows.h>
+#include <wrl.h>
+#define DIRECTINPUT_VERSION  0x0800 //DirectInputのバージョン指定
+#include <dinput.h>
 
 class Input
 {
+public:
+	//namespace省略
+	template<class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
 public://メンバ関数
 	//初期化
 	void Initialize(HINSTANCE hInstance,HWND hwnd);
 	//更新
 	void Update();
+private: //メンバ変数
+	//キーボードのデバイス
+	ComPtr<IDirectInputDevice8> keyboard;
+
 };
 
