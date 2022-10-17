ff01b76e055b30e63878cab0df8b0d528970bd98
niida_shuto <k021g1267@g.neec.ac.jp>
2022-10-17 14:53:12 +0900

    03_02まで終わり

---
+++ WinApp.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//WindowsAPI
+class WinApp
+{
+public://静的メンバ関数
+	static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+public://メンバ関数
+	//初期化
+	void Initialize();
+	//更新
+	void Update();
+
+};
