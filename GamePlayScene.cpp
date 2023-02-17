b8220ab43ef3da5843345c747ef92f015180c89a
niida_shuto <k021g1267@g.neec.ac.jp>
2023-02-17 10:15:17 +0900

    10_1途中まで

---
+++ GamePlayScene.cpp
@@ -0,0 +1,27 @@
+#include "GamePlayScene.h"
+
+void GamePlayScene::Initializa()
+{
+  
+    spriteCommon->LoadTexture(0, "texture.png");
+    spriteCommon->LoadTexture(1, "reimu.png");
+
+    Object3d::StaticInitialize(dxCommon->GetDevice(), WinApp::window_width, WinApp::window_height);
+
+    //‰¹º“Ç‚Ýž‚Ý
+    audio->SoundLoadWave("Resources/fanfare.wav");
+    //‰¹ºÄ¶
+    //audio->SoundPlayWave("Resources/fanfare.wav");
+}
+
+void GamePlayScene::Finalize()
+{
+}
+
+void GamePlayScene::Update()
+{
+}
+
+void GamePlayScene::Draw()
+{
+}
